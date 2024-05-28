1578a2fd440c739c87dde4f06ab6c303a8748c1d
ggwplank <116358472+ggwplank@users.noreply.github.com>
2024-05-28 09:46:25 +0200

    Delete ISW-Project directory

--- ISW-Project/src/main/java/Utils/Properties.java
+++
@@ -1,6 +0,0 @@
-package Utils;
-
-public class Properties {
-    public static final String OUTPUT_DIRECTORY = "C:/Users/bruno/IdeaProjects/ISW-Project/Outputs/";
-    public static final String APACHE_DIRECTORY = "C:/Users/bruno/IdeaProjects";
-}
